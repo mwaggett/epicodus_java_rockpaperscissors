eed11ef1106a088cb84cc6f79c5b39b5fe2ef22a
Daryl Seaver & Molly Waggett <student@epicodus.com>
2015-08-12 12:13:05 -0700

    Fill out main method to include spark server pages and display results of player vs computer.

--- src/main/java/Rockpaperscissors.java
+++ src/main/java/Rockpaperscissors.java
@@ -1,8 +1,41 @@
 import java.util.Random;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.Map;
 
 
 public class Rockpaperscissors {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) ->{
+      HashMap model = new HashMap();
+
+      model.put("template", "templates/playrockpaperscissors.vtl");
+      return new ModelAndView(model, layout);
+
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/results.vtl");
+
+      String player = request.queryParams("player");
+      model.put("player", player);
+      String computer = computerOutput();
+      model.put("computer", computer);
+      Boolean output = checkWinner(player, computer);
+
+      model.put("output", output);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+
 
   public static Boolean checkWinner(String player1, String player2) {
     //Does player1 win?
@@ -31,24 +64,38 @@ public class Rockpaperscissors {
     }
   }
 
-  public static Boolean checkWinnerVsComputer(String player) {
-    player = checkInput(player);
+  // public static Boolean checkWinnerVsComputer(String player) {
+  //   player = checkInput(player);
+  //
+  //
+  //   if ((player.equals("rock") && computer.equals(0)) ||
+  //       (player.equals("scissors") && computer.equals(1)) ||
+  //       (player.equals("paper") && computer.equals(2))) {
+  //         System.out.println("Player said "+ player + ". Computer said "+computer+".");
+  //         return true;
+  //   } else {
+  //     System.out.println("Player said "+ player + ". Computer said "+computer+".");
+  //         return false;
+  //   }
+  //
+  // }
+
+  public static String computerOutput() {
     Random randomGen = new Random();
     Integer computer = randomGen.nextInt(3);
 
     //0 = scissors
     //1 = paper
     //2 = rock
-
-    if ((player.equals("rock") && computer.equals(0)) ||
-        (player.equals("scissors") && computer.equals(1)) ||
-        (player.equals("paper") && computer.equals(2))) {
-          System.out.println("Player said "+ player + ". Computer said "+computer+".");
-          return true;
+    String computerString;
+    if (computer.equals(0)) {
+      computerString = "scissors";
+    } else if (computer.equals(1)) {
+      computerString = "paper";
     } else {
-      System.out.println("Player said "+ player + ". Computer said "+computer+".");
-          return false;
+      computerString = "rock";
     }
 
+    return computerString;
   }
 }
